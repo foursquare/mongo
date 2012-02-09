15f4f567038adf6eb0711b6af7eb780d251eff77
Jon Hoffman <jon@foursquare.com>
2012-02-09 21:19:51 +0000

    always preallocate the journal

--- db/dur_journal.cpp
+++ db/dur_journal.cpp
@@ -266,17 +266,8 @@ namespace mongo {
             return faster;
         }
         bool preallocateIsFaster() {
-            Timer t;
-            bool res = false;
-            if( _preallocateIsFaster() && _preallocateIsFaster() ) { 
-                // maybe system is just super busy at the moment? sleep a second to let it calm down.  
-                // deciding to to prealloc is a medium big decision:
-                sleepsecs(1);
-                res = _preallocateIsFaster();
-            }
-            if( t.millis() > 3000 ) 
-                log() << "preallocateIsFaster check took " << t.millis()/1000.0 << " secs" << endl;
-            return res;
+            log() << "preallocateIsFaster() hardcoded to 'true'" << endl;
+            return true;
         }
 
         // throws
